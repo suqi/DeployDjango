a116f9728432f17c7b73a59c36c5a3ca00b616b6
suqi <deqi.su@gmail.com>
2016-02-16 10:17:10 +0800

    copy nginx.conf to this repo

--- deploydjango.sh
+++ deploydjango.sh
@@ -2,7 +2,7 @@ sudo apt-get -y install python-pip
 sudo apt-get -y install nginx
 sudo apt-get -y install wget
 sudo pip install gunicorn 
-wget https://raw.githubusercontent.com/yask123/nginx-conf/master/nginx.conf
+wget https://raw.githubusercontent.com/suqi/DeployDjango/master/nginx.conf
 sudo cp nginx.conf /etc/nginx/nginx.conf
 sed -i -e 's?{{staticfilepath}}?'$PWD'/'$1'/collectstatic?g' nginx.conf
 sudo service nginx restart
